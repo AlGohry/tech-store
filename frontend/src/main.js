97b027c01cefa28911a93ed9437f01264aa2a150
AlGohry <ahmad.3bdellatif@gmail.com>
2025-04-30 14:40:10 +0300

    feat: add vue-toastification for enhanced notification support

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import axios from './plugins/axios' // Import axios from the plugins folder
+import Toast from 'vue-toastification' // Import Toast for notifications
+import 'vue-toastification/dist/index.css' // Import Toast CSS
 
 createApp(App).mount('#app')  // Import the main App component
 
@@ -13,7 +15,7 @@ const app = createApp(App)  // Create the Vue app instance
 // Set axios as a global property
 app.config.globalProperties.$axios = axios
 
-
+app.use(Toast); // Toast notifications
 app.use(createPinia())  // Pinia for state management
 app.use(router) // Vue Router for routing
 app.mount('#app') // Mount the app to the DOM
